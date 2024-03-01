e0a13e9d1f87bc92a0f66ee7323f07148069e505
Madhu Muppala <madhu4mobile@gmail.com>
2024-02-29 21:23:40 -0600

    ADV-JAVA - Second Challenge completed and verified

--- src/practice/Section4Challenge.java
+++ src/practice/Section4Challenge.java
@@ -3,19 +3,21 @@ package practice;
 public class Section4Challenge {
     public static void main(String[] args) {
 
-        Double myDouble = 20.00;
-        Double mySecondDouble = 80.00;
-        Double myResult = (myDouble + mySecondDouble) * 100;
+        Double myDouble = 25.00d;
+        Double mySecondDouble = 70.00d;
+        Double myResult = myDouble + mySecondDouble * 100.00d;
+        System.out.println("myResult = " + myResult);
 
-        Double myReminder = myResult % 40.00;
+        Double myReminder = myResult % 3;
+        System.out.println("myReminder when divided by 3 = " + myReminder);
 
-        boolean isReminder = ( myReminder != 0.00) ? (false) : (true);
+        boolean isReminder = ( myReminder != 0.00) ? false : true;
         System.out.println("isReminder = " + isReminder);
         
         if( isReminder){
-            System.out.println("Reminder is zero ! ");
+            System.out.println("Reminder is zero !");
         } else {
-            System.out.println("got some reminder and it is = " + myReminder);
+            System.out.println("Got some reminder and it is = " + myReminder);
         }
 
     }
